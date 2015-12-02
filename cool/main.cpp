76b865b93be72b1c7298fecc3e297168457c2fc5
godoing <20141105063T@mac04.local>
2015-12-02 21:05:56 +0800

    Initial Commit

---
+++ cool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cool
+//
+//  Created by 20141105063T on 15/12/2.
+//  Copyright © 2015年 20141105063T. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
